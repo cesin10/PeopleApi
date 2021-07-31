d37287ad8bbe04864c3568b6431905ecfd09cbfd
cesin10@hotmail.com <cesin10@hotmail.com>
2021-07-31 16:11:14 -0300

    DIO PersonApi Santander Bootcamp

---
+++ src/main/java/peopleapi/peopleapi.java
@@ -0,0 +1,13 @@
+package peopleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class peopleapi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(peopleapi.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/personapisantander/PersonapisantanderApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.personapisantander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapisantanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
