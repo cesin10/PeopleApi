bbc1570cdfbf79433389bbf1d6cfa318140bee16
cesin10@hotmail.com <cesin10@hotmail.com>
2021-08-03 17:21:25 -0300

    Developed update by id feature

--- src/main/java/peopleapi/controller/PeopleController.java
+++ src/main/java/peopleapi/controller/PeopleController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -50,9 +51,13 @@ public class PeopleController {
 	@DeleteMapping("/{id}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void deleteById(@PathVariable Long id) throws PersonNotFoundException {
-		personService.delete(id);
-		
+		personService.delete(id);		
 	
+	}
+	
+	@PutMapping("/{id}")
+	public MessageResponseDTO updateById (@PathVariable Long id, @RequestBody @Valid PersonDTO personDto) throws PersonNotFoundException {
+		return personService.updateById(id, personDto); 
 	}
 	
 
